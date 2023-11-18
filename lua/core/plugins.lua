f71487233e99df7e06cd86cd8bda2c38c81238f4
cesar <cesarmarcialq@gmail.com>
2023-11-18 14:46:45 -0800

    Finished setting up mason and lsp
    Followed this guide https://www.youtube.com/watch?v=lpQMeFph1RE&list=PLsz00TDipIffxsNXSkskknolKShdbcALR&index=2&pp=iAQB

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -17,12 +17,16 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
-
   use 'nvim-treesitter/nvim-treesitter'
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
     requires = {{ 'nvim-lua/plenary.nvim' }}
+  }
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig"
   }
 
   if packer_bootstrap then
